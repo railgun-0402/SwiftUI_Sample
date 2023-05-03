5554b677cb923fd35d6efa0038328f604d468bc8
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-05-03 23:43:14 +0900

    Initial Commit

---
+++ TestSwiftUI01/TestSwiftUI01App.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUI01App.swift
+//  TestSwiftUI01
+//
+//  Created by 菅原大輝 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUI01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestSwiftUI01UITests/TestSwiftUI01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TestSwiftUI01UITestsLaunchTests.swift
+//  TestSwiftUI01UITests
+//
+//  Created by 菅原大輝 on 2023/05/03.
+//
+
+import XCTest
+
+final class TestSwiftUI01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

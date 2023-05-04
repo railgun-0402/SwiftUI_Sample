24800bf22cb3b6de734ac6304b2fa6af64ea74e9
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-05-05 01:05:53 +0900

    初回作成

--- TestSwiftUI01/ContentView.swift
+++ TestSwiftUI01/ContentView.swift
@@ -1,23 +1,43 @@
-//
-//  ContentView.swift
-//  TestSwiftUI01
-//
-//  Created by 菅原大輝 on 2023/05/03.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countdownTime = 60
+    @State private var currentTime = 60
+    @State private var isRunning = false
+    @State private var timerType = "Countdown"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(currentTime)")
+                .font(.largeTitle)
+            Spacer().frame(height: 20)
+            Button(isRunning ? "Stop" : "Start") {
+                if isRunning {
+                    stopTimer()
+                } else {
+                    startTimer()
+                }
+            }
         }
-        .padding()
+    }
+    
+    func startTimer() {
+        isRunning = true
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            if currentTime == 0 {
+                stopTimer()
+            } else {
+                currentTime -= 1
+            }
+        }
+    }
+    
+    func stopTimer() {
+        isRunning = false
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
